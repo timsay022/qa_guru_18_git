9ffd363f5b26d95a6330eb181c8ad04df0ff90b9
timsay02 <57029479+timsay022@users.noreply.github.com>
2024-11-25 23:19:40 +0500

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -4,7 +4,7 @@ package guru.qa;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    private static String msg = "Hello QA Guru!!!!!!!";
+    private static String msg = "Hello QA Guru!";
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
