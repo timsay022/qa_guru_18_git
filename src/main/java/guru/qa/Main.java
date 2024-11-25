410acd0fbddf729692b326cff7a2f6326b0abe00
timsay02 <57029479+timsay022@users.noreply.github.com>
2024-11-25 23:20:38 +0500

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -4,13 +4,13 @@ package guru.qa;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    private static String msg = "Hello QA Guru!";
+    private static String str = "Hello QA Guru!";
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         
         
-        System.out.printf(msg);
+        System.out.printf(str);
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
